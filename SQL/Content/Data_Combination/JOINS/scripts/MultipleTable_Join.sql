53cea5a298e2e2935e149df1acf1090182914a8a
actionxbar-14 <ankitpaths144@gmail.com>
2026-09-16 17:58:14 +0530

    minor_changes!

--- SQL/Content/Data_Combination/JOINS/scripts/MultipleTable_Join.sql
+++ SQL/Content/Data_Combination/JOINS/scripts/MultipleTable_Join.sql
@@ -28,9 +28,15 @@ SELECT
     e.FirstName AS EmployeeFirstName,
     e.LastName AS EmployeeLastName
 FROM Sales.Orders AS o
-LEFT JOIN Sales.Customers AS c
-ON o.CustomerID = c.CustomerID
-LEFT JOIN Sales.Products AS p
-ON o.ProductID = p.ProductID
-LEFT JOIN Sales.Employees AS e
-ON o.SalesPersonID = e.EmployeeID
+LEFT JOIN 
+Sales.Customers AS c
+ON
+o.CustomerID = c.CustomerID
+LEFT JOIN
+Sales.Products AS p
+ON 
+o.ProductID = p.ProductID
+LEFT JOIN 
+Sales.Employees AS e
+ON 
+o.SalesPersonID = e.EmployeeID
